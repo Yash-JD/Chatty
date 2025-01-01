f4ce943abd3e990902fcbadc18fc2196e53e9254
Yash <yjdobariya0@gmail.com>
2025-01-02 00:57:56 +0530

    ckeck user done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -14,6 +14,6 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 router.put('update-profile', protectRoute, updateProfile);
-router.get('/check', protectRoute, checkAuth);
+router.get('/check', protectRoute, checkAuth); // let say in profile page we refresh then check if it is authenticated
 
 export default router;
