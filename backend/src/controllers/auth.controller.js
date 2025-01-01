ee05ae658949b1d4b0a0f67afb26e3f3fd681092
Yash <yjdobariya0@gmail.com>
2025-01-02 00:48:08 +0530

    update profile done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -81,6 +82,29 @@ export const logout = (req, res) => {
     res.status(200).json({ message: 'Logged out successfully' });
   } catch (error) {
     console.log('Error in logout controller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: 'Profile pic is required' });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const userId = req.user._id;
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true },
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.log('Error in updateProfile controller', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
