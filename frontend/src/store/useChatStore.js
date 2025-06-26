2169815c37e4190934777f66f87eec99ec820498
Yash <yjdobariya0@gmail.com>
2025-06-26 22:30:59 +0530

    adding new messages to socket

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand';
 import toast from 'react-hot-toast';
 import { axiosInstance } from '../lib/axios';
+import { useAuthStore } from './useAuthStore';
 
 export const useChatStore = create((set, get) => ({
   messages: [],
@@ -43,6 +44,24 @@ export const useChatStore = create((set, get) => ({
     } catch (error) {
       toast.error(error.response.data.message);
     }
+  },
+
+  // Subscribe to new messages from the socket
+  subscribeToMessages: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on('newMessage', (newMessage) => {
+      set({ messages: [...get().messages, newMessage] });
+    });
+  },
+
+  // when user logs out, unsubscribe from new messages
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off('newMessage');
   },
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
